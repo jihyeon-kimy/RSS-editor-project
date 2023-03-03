fd688fc085664239c956324865e3e11e1c51945e
jihyeon-kimy <jihyeon.rem@gmail.com>
2023-03-03 17:03:47 +0900

    Refactor: react-router-dom 단순 경로 > Outlet 사용하는 형태로 Router 변경

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -3,14 +3,17 @@ import Home from "../../pages/Home";
 import PageA from "../../pages/PageA";
 import PageB from "../../pages/PageB";
 import PageC from "../../pages/PageC";
+import Layout from "../Layout";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pageA" element={<PageA />} />
-      <Route path="/pageB" element={<PageB />} />
-      <Route path="/pageC" element={<PageC />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/pageA" element={<PageA />} />
+        <Route path="/pageB" element={<PageB />} />
+        <Route path="/pageC" element={<PageC />} />
+      </Route>
     </Routes>
   );
 };

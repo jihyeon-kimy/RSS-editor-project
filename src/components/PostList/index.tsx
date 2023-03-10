777491786b0f83b5c083f266377ae130f5a06f2a
jihyeon-kimy <jihyeon.rem@gmail.com>
2023-03-10 15:58:35 +0900

    Add: Post List컴포넌트 및 구독 목록 추가

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,23 +1,93 @@
+import { useEffect, useState } from "react";
 import RSSParser from "rss-parser";
+import styled from "styled-components";
+import color from "../../styles/color";
+import text from "../../styles/text";
+import subscribeList from "./subscribeList";
+import { customMedia } from "../../styles/GlobalStyle";
+import PostItem from "./PostItem";
 
 const PostList = () => {
-  let parser = new RSSParser();
+  const [isLoading, setIsLoading] = useState(true);
+  const [newPostList, setNewPostList] = useState<any[]>();
 
   // 일부 RSS  피드는 CORS 보안 때문에 브라우저에 로드할 수 없습니다.
   // 이 문제를 해결하려면 프록시를 사용할 수 있습니다.
   const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
-  parser.parseURL(
-    CORS_PROXY + "https://blog.zarathu.com/index.xml ",
-    function (err, feed) {
-      if (err) throw err;
-      console.log(feed);
-      // feed.items.forEach(function (entry) {
-      //   console.log(entry.title + ":" + entry.link);
-      // });
-    }
+  useEffect(() => {
+    const parser = new RSSParser();
+    setIsLoading(true);
+    (async () => {
+      let parsedPostList: any[] = [];
+      for (let subscribeItem of subscribeList) {
+        if (!subscribeItem.enabled) return;
+        try {
+          let parsedPost = await parser.parseURL(CORS_PROXY + subscribeItem.rssLink);
+          parsedPostList = [...parsedPostList, ...parsedPost.items];
+        } catch {
+          console.log(
+            `🚒삐뽀삐보🚒 ${subscribeItem.name} fetch 에러 발생! RSSLink 오타 혹은 CORS보안 이슈로 브라우저에 로드가 안되는 것인지 확인하세요!`
+          );
+        }
+      }
+      setNewPostList(parsedPostList);
+    })();
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <PostListContainer>
+      <Title>
+        <h3>피드 리스트</h3>
+        <strong>편하게 모아보는 꿀같은 피드</strong>
+      </Title>
+      <List>
+        {isLoading && <p>로딩중입니다...</p>}
+        {newPostList?.map((newPostItem, idx) => (
+          <PostItem
+            key={idx}
+            title={newPostItem?.title}
+            content={newPostItem[`content:encodedSnippet`] || newPostItem.contentSnippet}
+            author={newPostItem?.creator || newPostItem?.author}
+            date={newPostItem?.isoDate?.substr(0, 10)}
+          />
+        ))}
+      </List>
+    </PostListContainer>
   );
-  return <h1>This is Page A</h1>;
 };
 
 export default PostList;
+
+const PostListContainer = styled.section`
+  max-width: 1100px;
+  margin: 0 auto;
+`;
+
+const Title = styled.div`
+  padding: 50px 25px 30px;
+  font-weight: 600;
+
+  h3 {
+    ${text.textStyle24()};
+  }
+
+  strong {
+    ${text.textStyle18()};
+  }
+`;
+
+const List = styled.ol`
+  margin: 0 auto;
+  /* padding: 10px; */
+  margin: 10px;
+  background-color: ${color.white};
+  border: 1px solid ${color.border};
+  border-radius: 10px;
+
+  ${customMedia.lessThan("md")`
+    border-top: 1px solid ${color.border};
+    border-bottom: 1px solid ${color.border};
+  `}
+`;
---
+++ src/components/PostList/subscribeList.ts
@@ -0,0 +1,49 @@
+const subscribeList = [
+  {
+    name: "29CM",
+    rssLink: "https://medium.com/feed/29cm",
+    enabled: true,
+  },
+  {
+    name: "42Seoul",
+    rssLink: "https://42place.innovationacademy.kr/feed",
+    enabled: true,
+  },
+  {
+    name: "당근마켓",
+    rssLink: "https://medium.com/feed/daangn",
+    enabled: true,
+  },
+  {
+    name: "네이버 플레이스",
+    rssLink: "https://medium.com/feed/naver-place-dev",
+    enabled: true,
+  },
+  {
+    name: "리디북스",
+    rssLink: "http://www.ridicorp.com/story-category/tech-blog/feed/",
+    enabled: true,
+  },
+  {
+    name: "로켓펀치",
+    rssLink: "https://blog.rocketpunch.com/feed/",
+    enabled: true,
+  },
+  {
+    name: "마켓컬리",
+    rssLink: "https://helloworld.kurly.com/feed.xml",
+    enabled: true,
+  },
+  {
+    name: "뱅크샐러드",
+    rssLink: "https://blog.banksalad.com/rss.xml",
+    enabled: true,
+  },
+  {
+    name: "쏘카",
+    rssLink: "https://tech.socarcorp.kr/feed.xml",
+    enabled: true,
+  },
+];
+
+export default subscribeList;

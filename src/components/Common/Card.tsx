510228552598aa90eab957057505056b616a972c
jihyeon-kimy <jihyeon.rem@gmail.com>
2023-03-15 22:15:33 +0900

    Fix: 파일명 변경(PosrCard > Card : 범용적으로 사용하기 위해, 이름 수정)

--- src/components/Common/Card.tsx
+++ src/components/Common/Card.tsx
@@ -3,24 +3,27 @@ import styled from "styled-components";
 import color from "../../styles/color";
 import { customMedia } from "../../styles/GlobalStyle";
 
-interface PostCardProps {
+interface CardProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const PostCard = ({ children }: PostCardProps) => {
-  return <Card>{children}</Card>;
+const Card = ({ children, className }: CardProps) => {
+  return <CardContainer className={className}>{children}</CardContainer>;
 };
 
-export default PostCard;
+export default Card;
 
-const Card = styled.div`
+const CardContainer = styled.div`
   margin: 10px;
+  padding: 20px;
   background-color: ${color.white};
   border: 1px solid ${color.border};
   border-radius: 10px;
 
   ${customMedia.lessThan("md")`
     margin: 0;
+    padding: 10px;
     border-radius: 0;
     border-top: 1px solid ${color.border};
     border-bottom: 1px solid ${color.border};

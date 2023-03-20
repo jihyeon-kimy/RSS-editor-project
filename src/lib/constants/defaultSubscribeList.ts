e78eb9cd3e0865db2a5dfa1e47c55ee6728491ba
jihyeon-kimy <jihyeon.rem@gmail.com>
2023-03-20 14:20:44 +0900

    Refactor: default 구독리스트 페이지 위치 이동, id(uuid)추가

--- src/lib/constants/defaultSubscribeList.ts
+++ src/lib/constants/defaultSubscribeList.ts
@@ -1,35 +1,44 @@
+import uuid from "react-uuid";
+
 const SUBSCRIBE_LIST = [
   {
+    id: uuid(),
     name: "29CM",
     rssLink: "https://medium.com/feed/29cm",
     enabled: true,
   },
   {
+    id: uuid(),
     name: "42Seoul",
     rssLink: "https://42place.innovationacademy.kr/feed",
-    enabled: true,
+    enabled: false,
   },
   {
+    id: uuid(),
     name: "당근마켓",
     rssLink: "https://medium.com/feed/daangn",
     enabled: true,
   },
   {
+    id: uuid(),
     name: "네이버 플레이스",
     rssLink: "https://medium.com/feed/naver-place-dev",
     enabled: true,
   },
   {
+    id: uuid(),
     name: "리디북스",
     rssLink: "http://www.ridicorp.com/story-category/tech-blog/feed/",
     enabled: true,
   },
   {
+    id: uuid(),
     name: "로켓펀치",
     rssLink: "https://blog.rocketpunch.com/feed/",
     enabled: true,
   },
   {
+    id: uuid(),
     name: "쏘카",
     rssLink: "https://tech.socarcorp.kr/feed.xml",
     enabled: true,
